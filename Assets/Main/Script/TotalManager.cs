401701202eb6a9094387d753096878ce35122900
noori4543 <noori4543@gmail.com>
2024-07-04 15:28:06 +0900

    메인 메뉴(Start - 씬 랜덤 넘기기, Exit), 옵션(Esc - Resume, Volume, Exit)

---
+++ Assets/Main/Script/TotalManager.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class TotalManager : MonoBehaviour
+{
+    public static TotalManager instance;
+    
+    public Image fadeScreen;
+    public Image optionScreen;
+    
+    public Button optionGameExitButton;
+    public Button optionResumeButton;
+    private bool optionEnabled = false;
+    
+    public TextMeshProUGUI volumeText;
+    public AudioMixer mixer;
+    public Slider volumeSlider;
+    private float perVolume;
+    
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        volumeSlider.onValueChanged.AddListener(SetLevel);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (optionEnabled)
+            {
+                optionScreen.GameObject().SetActive(false);
+                optionEnabled = false;
+            }
+            else
+            {
+                optionScreen.GameObject().SetActive(true);
+                optionEnabled = true;
+            }
+        }
+    }
+    
+    public void MoveScene(int id)
+    {
+        StartCoroutine(MoveSceneWithFade(id));
+    }
+
+    public void MoveScene(string sceneName)
+    {
+        StartCoroutine(MoveSceneWithFade(sceneName));
+    }
+
+    private IEnumerator MoveSceneWithFade(int id)
+    {
+        yield return StartCoroutine(FadeScreen(true));// yield return이 Coroutine이 끝날때까지 기다림
+        SceneManager.LoadScene(id);
+        yield return StartCoroutine(FadeScreen(false));
+    }
+    
+    private IEnumerator MoveSceneWithFade(string sceneName)
+    {
+        yield return StartCoroutine(FadeScreen(true));// yield return이 Coroutine이 끝날때까지 기다림
+        SceneManager.LoadScene(sceneName);
+        yield return StartCoroutine(FadeScreen(false));
+    }
+
+    private IEnumerator FadeScreen(bool fadeOut)
+    {
+        var fadeTimer = 0f;
+        const float FadeDuration = 1f;
+
+        var initialValue = fadeOut ? 0f : 1f;
+        var fadeDir = fadeOut ? 1f : -1f;
+        
+        while (fadeTimer < FadeDuration)
+        {
+            yield return null; //한 프레임만 기다려서 업데이트처럼 프레임당 움직임
+            fadeTimer += Time.deltaTime;
+
+            var color = fadeScreen.color;
+
+            initialValue += fadeDir * Time.deltaTime;
+            color.a = initialValue;
+
+            fadeScreen.color = color;
+        }
+    }
+
+    public void SetVolumeText()
+    {
+        perVolume = volumeSlider.value * 100.0f;
+        volumeText.text = perVolume.ToString("N0") + "%";
+    }
+
+    public void SetLevel(float sliderVal) {
+        mixer.SetFloat("BGM", Mathf.Log10(sliderVal)*20);
+    }
+    
+    public void ResumeGame()
+    {
+        optionScreen.GameObject().SetActive(false);
+        optionEnabled = false;
+    }
+
+    public void ExitGame()
+    {
+#if  UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
