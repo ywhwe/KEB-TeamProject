b5a98606fa4b3181d6c23281e71897ece7ecaede
noori4543 <noori4543@gmail.com>
2024-07-05 14:02:00 +0900

    버튼 사운드 이펙트 적용, MainMenu 스크립트 TotalManager 스크립트로 병합, UI 해상도에 따른 크기 조정

--- Assets/Main/Script/TotalManager.cs
+++ Assets/Main/Script/TotalManager.cs
@@ -8,10 +8,13 @@ using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class TotalManager : MonoBehaviour
 {
     public static TotalManager instance;
+
+    public Button gameStartButton;
     
     public Image fadeScreen;
     public Image optionScreen;
@@ -24,12 +27,18 @@ public class TotalManager : MonoBehaviour
     public AudioMixer mixer;
     public Slider volumeSlider;
     private float perVolume;
+
+    private float readyTimer = 1f;
+    private float readyTime = 0f;
+    private int startTime = 0;
+    
     
     private void Awake()
     {
         instance = this;
         DontDestroyOnLoad(gameObject);
         volumeSlider.onValueChanged.AddListener(SetLevel);
+        Ready();
     }
 
     public void Update()
@@ -52,6 +61,7 @@ public class TotalManager : MonoBehaviour
     public void MoveScene(int id)
     {
         StartCoroutine(MoveSceneWithFade(id));
+
     }
 
     public void MoveScene(string sceneName)
@@ -64,6 +74,7 @@ public class TotalManager : MonoBehaviour
         yield return StartCoroutine(FadeScreen(true));// yield return이 Coroutine이 끝날때까지 기다림
         SceneManager.LoadScene(id);
         yield return StartCoroutine(FadeScreen(false));
+        
     }
     
     private IEnumerator MoveSceneWithFade(string sceneName)
@@ -104,6 +115,19 @@ public class TotalManager : MonoBehaviour
     public void SetLevel(float sliderVal) {
         mixer.SetFloat("BGM", Mathf.Log10(sliderVal)*20);
     }
+
+    public void ButtonSFX()
+    {
+        AudioSource buttonSFX = GetComponent<AudioSource>();
+        buttonSFX.Play();
+    }
+
+    public void GoToIngame()
+    {
+        int gameNumber = Random.Range(1, 3);
+        TotalManager.instance.MoveScene(gameNumber);
+        gameStartButton.interactable = false;
+    }
     
     public void ResumeGame()
     {
@@ -119,5 +143,4 @@ public class TotalManager : MonoBehaviour
         Application.Quit();
 #endif
     }
-
 }
