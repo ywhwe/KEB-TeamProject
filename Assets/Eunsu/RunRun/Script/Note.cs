49654dd4cc3ea7a29f1cdfba23a3269ba7e5b9ad
ywhwe <esromer@kyonggi.ac.kr>
2024-07-08 13:13:26 +0900

    Reset before merging

--- Assets/Eunsu/RunRun/Script/Note.cs
+++ Assets/Eunsu/RunRun/Script/Note.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 public class Note : MonoBehaviour
 {
     private RectTransform _rectTransform;
-    private WaitForSeconds hmm = new (0.01f);
+    private WaitForSeconds hmm = new(0.01f);
     private float noteSpeed = 3f;
     private float movePos;
-    
+
     private float durationTime = 0f;
 
     private void Awake()
     {
         _rectTransform = GetComponent<RectTransform>();
     }
-    
+
     void Start()
     {
         StartCoroutine(MoveNote());
-        
+
     }
-    
+
     void Update()
     {
         durationTime += Time.deltaTime;
@@ -34,7 +34,7 @@ public class Note : MonoBehaviour
         while (true)
         {
             yield return hmm;
-            
+
             movePos -= noteSpeed;
             _rectTransform.anchoredPosition = new Vector2(movePos, _rectTransform.anchoredPosition.y);
             // gameObject.transform.Translate(-1f * (noteSpeed * Time.deltaTime), 0f, 0f);
