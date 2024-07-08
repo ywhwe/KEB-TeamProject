65747ce72ac559b917aeb87db1eec3143d493c98
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-07-08 14:49:33 +0900

    Record 수정

--- Assets/Kanghyeon/RecordPlay/Script/NoteSpawner.cs
+++ Assets/Kanghyeon/RecordPlay/Script/NoteSpawner.cs
@@ -6,7 +6,10 @@ using Random = UnityEngine.Random;
 
 public class NoteSpawner : MonoBehaviour
 {
-    public GameObject note;
+    public GameObject noteW;
+    public GameObject noteA;
+    public GameObject noteS;
+    public GameObject noteD;
     public Transform noteparents;
     public Records record;
     
@@ -41,7 +44,7 @@ public class NoteSpawner : MonoBehaviour
     {
         var setha = Random.Range(0f, 360) * (Mathf.PI / 180);
         var position = new Vector3(Mathf.Cos(setha)*2.2f,0.08f,Mathf.Sin(setha)*2.2f);
-        var newnote = Instantiate(note, position, Quaternion.identity);
+        var newnote = Instantiate(noteW, position, Quaternion.identity);
         newnote.transform.SetParent(noteparents);
 
     }
@@ -49,7 +52,7 @@ public class NoteSpawner : MonoBehaviour
     {
         var setha = Random.Range(0f, 360) * (Mathf.PI / 180);
         var position = new Vector3(Mathf.Cos(setha)*2.7f,0.08f,Mathf.Sin(setha)*2.7f);
-        var newnote = Instantiate(note, position, Quaternion.identity);
+        var newnote = Instantiate(noteA, position, Quaternion.identity);
         newnote.transform.SetParent(noteparents);
 
     }
@@ -57,7 +60,7 @@ public class NoteSpawner : MonoBehaviour
     {
         var setha = Random.Range(0f, 360) * (Mathf.PI / 180);
         var position = new Vector3(Mathf.Cos(setha)*3.2f,0.08f,Mathf.Sin(setha)*3.2f);
-        var newnote = Instantiate(note, position, Quaternion.identity);
+        var newnote = Instantiate(noteS, position, Quaternion.identity);
         newnote.transform.SetParent(noteparents);
 
     }
@@ -65,7 +68,7 @@ public class NoteSpawner : MonoBehaviour
     {
         var setha = Random.Range(0f, 360) * (Mathf.PI / 180);
         var position = new Vector3(Mathf.Cos(setha)*3.71f,0.08f,Mathf.Sin(setha)*3.71f);
-        var newnote = Instantiate(note, position, Quaternion.identity);
+        var newnote = Instantiate(noteD, position, Quaternion.identity);
         newnote.transform.SetParent(noteparents);
 
     }
