7cd47476fb642c83b2947403d9d6659fd010e996
ywhwe <esromer@kyonggi.ac.kr>
2024-07-23 10:31:38 +0900

    HitFX added
    1. Added hit effect is activated on note clear
    2. Deleted unused prefabs

--- Assets/Eunsu/RunRun/Script/ButtonController.cs
+++ Assets/Eunsu/RunRun/Script/ButtonController.cs
@@ -12,6 +12,8 @@ public class ButtonController : MonoBehaviour
     public Sprite pressedImage;
     
     private Vector2 hitboxSize;
+
+    [SerializeField] private GameObject hitFX;
     
     public KeyCode keyToPress;
 
@@ -59,8 +61,11 @@ public class ButtonController : MonoBehaviour
                 }
                 
                 SoundManager.instance.PlaySound("ClearNote");
+                var fx = Instantiate(hitFX, judge.transform.position, Quaternion.identity);
                 
                 Destroy(judge.gameObject);
+                Destroy(fx,0.5f);
+                
                 NoteController.instance.noteCount--;
                 scoreBoard.SetScore(noteScore);
             }
