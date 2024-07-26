e9c485c774d46bd09e53411886a4980ca0fef25b
noori4543 <noori4543@gmail.com>
2024-07-26 15:41:15 +0900

    bass ball 모델링, Follow the Motion Life UI 추가, GameManagerFTM 정리

--- Assets/Jisoo/Script/GameManagerFTM.cs
+++ Assets/Jisoo/Script/GameManagerFTM.cs
@@ -3,23 +3,96 @@ using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class GameManagerFTM : WholeGameManager
 {
     public static GameManagerFTM instance;
     
+    public int playerLife = 3;
+    public Image[] lifeImage;
+    
     public float startTime;
     public float finishTime;
     public PhotonView PV;
     
+    protected WaitForSeconds calTime = new WaitForSeconds(5f);
+    public GameObject player;
+    protected int player1Motion;
+    
     private void Awake()
     {
         instance = this;
-        NetworkManager.instance.isDescending = true;
+        
     }
 
+    private void Start()
+    {
+        player = CreatePlayer.instance.player1;
+        NetworkManager.instance.isDescending = true;
+    }
+    
+    void Update()
+    {
+        player1Motion = player.GetComponent<CharacterControl>().motionNumber;
+    }
+
+    public IEnumerator PlayGameRoutine()
+    {
+        RandomMotion.instance.RandomAction();
+
+        yield return calTime;
+        
+        playerLife = RandomMotion.instance.CompareMotionNumber(player1Motion, playerLife);
+        LifeImageDelete();
+        
+        if (playerLife == 0)
+        {
+            EndGame();
+        }
+        else
+        {
+            StartCoroutine(PlayGameRoutine());   
+        }
+    }
+
+    public void LifeImageDelete()
+    {
+        switch (playerLife)
+        {
+            case 2:
+                lifeImage[playerLife].gameObject.SetActive(false);
+                break;
+            case 1:
+                lifeImage[playerLife].gameObject.SetActive(false);
+                break;
+            case 0:
+                lifeImage[playerLife].gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    public void StartGame()
+    {
+        startTime = Time.time;
+        
+        StartCoroutine(PlayGameRoutine());
+    }
+
+    public void EndGame()
+    {
+        Time.timeScale = 1f;
+        RandomMotion.instance.stage = 0;
+        finishTime = Time.time;
+        score = finishTime - startTime;
+        TotalManager.instance.StartFinish();
+    }
+    
     public override void GameStart()
     {
-        RandomMotion.instance.StartGame();
+        StartGame();
     }
+    
+    
 }
