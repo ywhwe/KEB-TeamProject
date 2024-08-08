e48150ea90572316b81c31265ea0e96863b23687
ywhwe <esromer@kyonggi.ac.kr>
2024-08-08 14:10:38 +0900

    Additional button generation
    WIP

--- Assets/Eunsu/BtnAction/Script/GameManagerBtn.cs
+++ Assets/Eunsu/BtnAction/Script/GameManagerBtn.cs
@@ -1,10 +1,10 @@
+using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 using Random = UnityEngine.Random;
 using Photon.Pun;
-using UnityEngine.UI;
 
 public class GameManagerBtn : WholeGameManager
 {
@@ -18,13 +18,17 @@ public class GameManagerBtn : WholeGameManager
     [Header("Trolley")]
     public GameObject trolleyPrefab;
     private readonly Vector3 trolleyPos = new (2f, 0.44f, -0.197f);
-
+    
+    [ArrayElementTitle("position")]
+    [Header("Buttons")]
+    [SerializeField] private Buttons[] genParts;
+    
     [Header("Buttons")]
     [SerializeField] private GameObject W;
     [SerializeField] private GameObject A;
     [SerializeField] private GameObject S;
     [SerializeField] private GameObject D;
-    private GameObject waitingKey;
+    private GameObject[] waitingKey;
 
     [Header("Counter")]
     public TextMeshProUGUI timeCounter;
@@ -49,6 +53,8 @@ public class GameManagerBtn : WholeGameManager
     [HideInInspector] public bool isGen; // This will be true if button has generated
 
     private bool isAccel;
+
+    private int level;
     
     public PhotonView PV;
 
@@ -58,6 +64,7 @@ public class GameManagerBtn : WholeGameManager
         TotalManager.instance.SendMessageSceneStarted();
         successCount = 0;
         clearTime = 0;
+        level = 0;
         
         successCounter.text = successCount.ToString();
         timeCounter.text = StartTime.ToString("F2");
@@ -85,16 +92,25 @@ public class GameManagerBtn : WholeGameManager
         {
             await UniTask.WaitUntil(() => !isAccel);
             
-            rand = Random.Range(0, 100);
-            BtnControl(rand);
+            BtnControl();
             
             // if IsMatch is false, suspends coroutine 'til it is true
             await UniTask.WaitUntil(() => isMatch);
 
             successCount++;
+
+            level = successCount switch
+            {
+                >= 15 and < 35 => 1,
+                >= 35 => 2,
+                _ => level
+            };
+            
             successCounter.text = successCount.ToString();
             
-            waitingKey.SetActive(false);
+            waitingKey[0].SetActive(false);
+            waitingKey[1].SetActive(false);
+            waitingKey[2].SetActive(false);
             
             if (successCount is NumberOfButtons) break;
         }
@@ -102,33 +118,18 @@ public class GameManagerBtn : WholeGameManager
         isGameEnd = true;
     }
     
-    private void BtnControl(float random)
+    private void BtnControl()
     {
-        switch (random)
+        switch (level)
         {
-            case >= 0 and < 25 :
-                W.SetActive(true);
-                waitingKey = W;
-                waitingKeyCode = KeyCode.W;
-                isGen = true;
+            case 0:
+                genLogic();
                 break;
-            case >= 25 and < 50 :
-                A.SetActive(true);
-                waitingKey = A;
-                waitingKeyCode = KeyCode.A;
-                isGen = true;
+            case 1:
+                genLogic(level);
                 break;
-            case >= 50 and <75 :
-                S.SetActive(true);
-                waitingKey = S;
-                waitingKeyCode = KeyCode.S;
-                isGen = true;
-                break;
-            case >= 75 and < 100 :
-                D.SetActive(true);
-                waitingKey = D;
-                waitingKeyCode = KeyCode.D;
-                isGen = true;
+            case 2:
+                genLogic(level, true);
                 break;
         }
     }
@@ -208,6 +209,113 @@ public class GameManagerBtn : WholeGameManager
             await UniTask.Yield();
         }
     }
+
+    private void genLogic()
+    {
+        float random = Random.Range(0, 100);
+        
+        switch (random)
+        {
+            case >= 0 and < 25 :
+                genParts[1].W.SetActive(true);
+                waitingKey[1] = W;
+                waitingKeyCode = KeyCode.W;
+                isGen = true;
+                break;
+            case >= 25 and < 50 :
+                genParts[1].A.SetActive(true);
+                waitingKey[1] = A;
+                waitingKeyCode = KeyCode.A;
+                isGen = true;
+                break;
+            case >= 50 and <75 :
+                genParts[1].S.SetActive(true);
+                waitingKey[1] = S;
+                waitingKeyCode = KeyCode.S;
+                isGen = true;
+                break;
+            case >= 75 and < 100 :
+                genParts[1].D.SetActive(true);
+                waitingKey[1] = D;
+                waitingKeyCode = KeyCode.D;
+                isGen = true;
+                break;
+        }
+    }
+    
+    private void genLogic(int hmm)
+    {
+        for (var i = 1; i < 3; i++)
+        {
+            var j = 2 * i - 2;
+            
+            float random = Random.Range(0, 100);
+
+            switch (random)
+            {
+                case >= 0 and < 25 :
+                    genParts[j].W.SetActive(true);
+                    waitingKey[j] = W;
+                    waitingKeyCode = KeyCode.W;
+                    isGen = true;
+                    break;
+                case >= 25 and < 50 :
+                    genParts[j].A.SetActive(true);
+                    waitingKey[j] = A;
+                    waitingKeyCode = KeyCode.A;
+                    isGen = true;
+                    break;
+                case >= 50 and <75 :
+                    genParts[j].S.SetActive(true);
+                    waitingKey[j] = S;
+                    waitingKeyCode = KeyCode.S;
+                    isGen = true;
+                    break;
+                case >= 75 and < 100 :
+                    genParts[j].D.SetActive(true);
+                    waitingKey[j] = D;
+                    waitingKeyCode = KeyCode.D;
+                    isGen = true;
+                    break;
+            }
+        }
+    }
+    
+    private void genLogic(int hmm, bool hmmm)
+    {
+        for (var i = 0; i < 2; i++)
+        {
+            float random = Random.Range(0, 100);
+
+            switch (random)
+            {
+                case >= 0 and < 25 :
+                    genParts[i].W.SetActive(true);
+                    waitingKey[i] = W;
+                    waitingKeyCode = KeyCode.W;
+                    isGen = true;
+                    break;
+                case >= 25 and < 50 :
+                    genParts[i].A.SetActive(true);
+                    waitingKey[i] = A;
+                    waitingKeyCode = KeyCode.A;
+                    isGen = true;
+                    break;
+                case >= 50 and <75 :
+                    genParts[i].S.SetActive(true);
+                    waitingKey[i] = S;
+                    waitingKeyCode = KeyCode.S;
+                    isGen = true;
+                    break;
+                case >= 75 and < 100 :
+                    genParts[i].D.SetActive(true);
+                    waitingKey[i] = D;
+                    waitingKeyCode = KeyCode.D;
+                    isGen = true;
+                    break;
+            }
+        }
+    }
     
     public override void GameStart()
     {
@@ -241,4 +349,22 @@ public class GameManagerBtn : WholeGameManager
         
         TotalManager.instance.StartFinish();
     }
+}
+
+[Serializable]
+public struct Buttons
+{
+    public enum Position
+    {
+        Left,
+        Mid,
+        Right
+    }
+
+    [SerializeField] private Position position;
+    
+    [SerializeField] public GameObject W;
+    [SerializeField] public GameObject A;
+    [SerializeField] public GameObject S;
+    [SerializeField] public GameObject D;
 }
